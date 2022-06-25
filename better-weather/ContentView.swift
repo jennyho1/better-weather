8c57c3e6dab89928b0bc97ba08572a06f2089278
jennyho1 <jennhiho@gmail.com>
2022-06-25 03:18:56 -0400

    project setup

---
+++ better-weather/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  better-weather
+//
+//  Created by Jenny Ho on 2022-06-25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ better-weather/better_weatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  better_weatherApp.swift
+//  better-weather
+//
+//  Created by Jenny Ho on 2022-06-25.
+//
+
+import SwiftUI
+
+@main
+struct better_weatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
